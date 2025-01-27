02e700a6064a88773080392c8d5bad7452f64700
whrksp126 <whrksp126@gmail.com>
2025-01-27 18:41:27 +0900

    save

--- heyvoca_back/app.py
+++ heyvoca_back/app.py
@@ -1,7 +1,9 @@
+import os
 from app import create_app, db
 from flask_login import LoginManager
 from flask_cors import CORS
 
 app = create_app()
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000, debug=True)
+  port = int(os.getenv("FLASK_RUN_PORT", 5003))
+  app.run(host='0.0.0.0', port=port, debug=False)
