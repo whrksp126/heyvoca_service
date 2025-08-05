ba430b9ad47da52e0a35382e9de1e01ca7cf39c7
whrksp126 <whrksp126@gmail.com>
2025-08-06 02:02:41 +0900

    first commit

--- heyvoca_app/android/app/src/main/java/com/ghmate/heyvoca/MainActivity.kt
+++ heyvoca_app/android/app/src/main/java/com/ghmate/heyvoca/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.heyvoca
+package com.ghmate.heyvoca
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- heyvoca_app/android/app/src/main/java/com/ghmate/heyvoca/MainApplication.kt
+++ heyvoca_app/android/app/src/main/java/com/ghmate/heyvoca/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.heyvoca
+package com.ghmate.heyvoca
 
 import android.app.Application
 import com.facebook.react.PackageList
