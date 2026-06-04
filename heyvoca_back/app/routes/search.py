6641f926898ad7e6bdf9f81f9e8c02366a1f7edd
whrksp126 <whrksp126@gmail.com>
2026-06-04 17:51:43 +0900

    search: /search/bookstore JOIN 조인키를 새 컬럼명으로 변경
    bookstore_category 마이그레이션으로 name 컬럼이 category로 변경됨.
    search.py의 raw SQL JOIN이 옛 컬럼명을 그대로 써서 prod에서 500 발생.

--- heyvoca_back/app/routes/search.py
+++ heyvoca_back/app/routes/search.py
@@ -343,7 +343,7 @@ def search_bookstore_all():
             COALESCE(bc.sort_order, 999) AS category_sort_order
         FROM heyvoca_dict.bookstore bs
         JOIN heyvoca_dict.admin_voca_book avb ON bs.admin_voca_book_id = avb.id
-        LEFT JOIN heyvoca_dict.bookstore_category bc ON bc.name = bs.category
+        LEFT JOIN heyvoca_dict.bookstore_category bc ON bc.category = bs.category
         GROUP BY bs.id
         ORDER BY category_sort_order ASC, bs.id ASC
     """)
